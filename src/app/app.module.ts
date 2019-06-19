2fc8ae3761168386d04e7ef17c9e1638347aafcb
bhandhariya <sainiraja59@gmail.com>
2019-06-19 13:25:21 +0530

    firebase module integreted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
+
+//component and modules 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//firebase 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { environment } from 'src/environments/environment';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,AngularFireDatabaseModule,AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
