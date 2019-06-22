2cea5b4c596b32f23d9d3327c15da751c76051ca
bhandhariya <sainiraja59@gmail.com>
2019-06-22 10:33:13 +0530

    22 -june first commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as swal from "sweetalert";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  name;email;password;rpassword;
   constructor() { }
 
   ngOnInit() {
   }
+  register(){
+    var obj={
+      name:this.name,
+      email:this.email,
+      password:this.password,
+      rpassword:this.rpassword
+    }
+    console.log(obj)
+    if(obj.password != obj.rpassword){
+      swal("Are you sure?", {
+        dangerMode: true,
+        buttons: true,
+      });
+      
+      
 
+    }else{
+      
+    }
+  }
 }
