e035c57316a7ad087f60f525bfcc886ff7eaed08
bhandhariya <sainiraja59@gmail.com>
2019-06-25 16:57:25 +0530

    error fixed

--- src/app/dashboard/shared/sidebar/sidebar.component.ts
+++ src/app/dashboard/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  goToFirstComponent(){
+    if(this.router.url == 'dashboard'){
+      this.router.navigate(['../dashboard/first']);
+    }else{
+      this.router.navigate(['../../dashboard/first']);
+    }
+  }
+  goTopatientTableComponent(){
+    if(this.router.url == 'dashboard'){
+      this.router.navigate(['../dashboard/first']);
+    }else{
+      this.router.navigate(['../../dashboard/first']);
+    }
+  }
 }
