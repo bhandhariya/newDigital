33eca56f8396c0b2918cd76cc3ac382057f36531
bhandhariya <sainiraja59@gmail.com>
2019-06-25 12:41:04 +0530

    dashboard routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginGuard } from './shared/login.guard';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'register',component:RegisterComponent}
+  {path:'register',component:RegisterComponent},
+  {path:'dashboard', loadChildren:'./dashboard/dashboard.module#DashboardModule',canActivate:[LoginGuard]},
+  {path:'*', redirectTo:'login', pathMatch:'full'},
 ];
 
 @NgModule({
