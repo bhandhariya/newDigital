13d41daa465576b7d591d20286ebe269d328dc0e
bhandhariya <sainiraja59@gmail.com>
2019-06-25 16:56:00 +0530

    first compoenent routing cretaed

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,15 +5,17 @@ import {Routes,RouterModule} from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { TestComponent } from './test/test.component';
+import { FirstComponent } from './first/first.component';
 
 const AppRoutes :Routes = [
   { path : '' , component: DashboardComponent},
   { path : 'dashboard' , component: DashboardComponent},
   { path : '*' ,redirectTo:'dashboard',pathMatch:'full'},
+  {path:'first',component:FirstComponent}
 ]
 
 @NgModule({
-  declarations: [DashboardComponent, NavbarComponent, SidebarComponent, TestComponent],
+  declarations: [DashboardComponent, NavbarComponent, SidebarComponent, TestComponent, FirstComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(AppRoutes)
