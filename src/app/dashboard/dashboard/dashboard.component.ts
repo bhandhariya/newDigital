26b95dd0f2295759a804521f5017ea04253a112e
bhandhariya <sainiraja59@gmail.com>
2019-06-26 17:57:02 +0530

    Activated route added for send uid

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShareuidService } from "../shared/service/shareuid.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  uid;
+  constructor(private arout:ActivatedRoute,private share:ShareuidService) { }
 
   ngOnInit() {
+    this.arout.paramMap.subscribe(r=>{
+      this.uid=(r.get('uid'));
+      console.log(this.uid)
+      this.share.setUID(this.uid)
+    })
   }
 
 }
