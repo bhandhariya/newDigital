713048101a2f523b026f0478b5fb569ac04f7f25
bhandhariya <sainiraja59@gmail.com>
2019-06-26 17:57:34 +0530

    getUserNameByUid function added

--- src/app/dashboard/shared/navbar/navbar.component.ts
+++ src/app/dashboard/shared/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareuidService } from "../service/shareuid.service";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private share:ShareuidService,private auth:AngularFireAuth,private http:HttpClient) { }
+  uid;username;
   ngOnInit() {
+    this.uid=this.share.getUID();
+    console.log(this.uid)
+    this.getUserNameByUid()
+   
+  }
+  getUserNameByUid(){
+    var obj={
+      id:this.uid
+    }
+    console.log(obj)
+    this.http.post('http://localhost:3000/api/users/getDataById',obj).subscribe(this.cb)
+  }
+  cb=(dt)=>{
+    console.log(dt)
+    this.username=dt.name;
   }
 
 }
