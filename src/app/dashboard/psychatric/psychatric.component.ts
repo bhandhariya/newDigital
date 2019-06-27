bd8c49317baeae35b99b07731114ef1dba84a047
bhandhariya <sainiraja59@gmail.com>
2019-06-27 15:36:56 +0530

    PsychatricComponent created

---
+++ src/app/dashboard/psychatric/psychatric.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-psychatric',
+  templateUrl: './psychatric.component.html',
+  styleUrls: ['./psychatric.component.css']
+})
+export class PsychatricComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
