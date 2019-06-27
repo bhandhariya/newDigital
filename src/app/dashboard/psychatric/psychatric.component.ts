386f898af972bd6c52f1b3e8e1dcc46882b91f06
bhandhariya <sainiraja59@gmail.com>
2019-06-27 16:03:13 +0530

    Accordian implemented successfully

--- src/app/dashboard/psychatric/psychatric.component.ts
+++ src/app/dashboard/psychatric/psychatric.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./psychatric.component.css']
 })
 export class PsychatricComponent implements OnInit {
+  step = 0;
 
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
   constructor() { }
 
   ngOnInit() {
