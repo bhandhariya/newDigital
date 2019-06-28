ee8aa9067c1c48400eba2c3cc0f057a03a31d50a
bhandhariya <sainiraja59@gmail.com>
2019-06-28 11:42:03 +0530

    all patient showign component

--- src/app/dashboard/all-pat/all-pat.component.ts
+++ src/app/dashboard/all-pat/all-pat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-all-pat',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-pat.component.css']
 })
 export class AllPatComponent implements OnInit {
-
-  constructor() { }
+  
+  Patient;
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+  this.getAllPatientDetails();
+  }
+  
+  getAllPatientDetails(){
+    this.http.get('https://digitalapp001.herokuapp.com/api/pat/getall').subscribe(this.cb)
+    
+  } 
+  cb=(dt)=>{
+    console.log(dt)
+   this.Patient=dt;
+   
   }
 
 }
+
+
+
